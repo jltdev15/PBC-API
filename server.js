a29a699dc1ae20fbccb627e73007dc94462672a1
jltdev15 <75570239+jltdev15@users.noreply.github.com>
2024-02-27 22:53:32 +0800

    change uri

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const studentRoutes = require("./routes/studentRoutes");
-const adminRoutes = require('./routes/adminRoutes');
+const adminRoutes = require("./routes/adminRoutes");
 require("dotenv").config({ path: "config.env" });
 const port = process.env.PORT || 3000;
-const db = process.env.DATABASE_LOCAL;
+const db = process.env.DATABASE_URI;
 
 const app = express();
 app.use(cookieParser());
 app.set("view engine", "ejs");
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -28,9 +28,8 @@ app.use(
       "http://localhost:3000",
       "http://localhost:8080",
       "http://localhost:5173",
-      "https://ictportal.vercel.app",
       "http://localhost:5174",
-      "https://ictportaledu.vercel.app",
+      "https://online-document-request-system.vercel.app/",
     ],
     credentials: true,
   })
