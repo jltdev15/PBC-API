33d36e57166adf3fabd710d518ed3a488a09ce1d
jltdev15 <75570239+jltdev15@users.noreply.github.com>
2024-03-04 21:46:15 +0800

    check duplicate in lrn

--- middleware/errorController.js
+++ middleware/errorController.js
@@ -1,8 +1,17 @@
 const Learner = require("../model/LearnerModel");
+const User = require("../model/UserModel");
 
 exports.checkLRN = async (req, res, next) => {
   const { learnerReferenceNumber } = req.body;
   try {
+    const checkDuplicate = await User.exists({
+      learnerReferenceNumber: learnerReferenceNumber,
+    });
+    if (checkDuplicate) {
+      return res.status(404).json({
+        content: "LRN already registered",
+      });
+    }
     const lrnFound = await Learner.exists({
       learnerReferenceNumber: learnerReferenceNumber,
     }).exec();
